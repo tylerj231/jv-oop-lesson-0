0787442043640dae0a740d726b3128fccdb2519d
tylerj231 <jacktyler2391@gmail.com>
2025-07-26 17:29:06 +0300

    Refactor on mentor's feedback

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -3,13 +3,10 @@ package mate.academy.service;
 import mate.academy.model.User;
 
 public class AuthenticationService {
+    private UserService userService = new UserService();
 
     public boolean login(String email, String password) {
-        User user = new UserService().findByEmail(email);
-
-        if (user != null && user.getPassword().equals(password)) {
-            return true;
-        }
-        return false;
+        User currentUser = userService.findByEmail(email);
+        return currentUser != null && currentUser.getPassword().equals(password);
     }
 }
